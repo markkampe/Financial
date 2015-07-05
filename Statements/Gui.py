741dde59bdb17261c802496bd6d31b51103afad4
Mark.Kampe <mark.kampe@gmail.com>
2015-07-05 10:09:24 -0700

    Statements: implement the accept and delete buttons in the GUI

--- Statements/Gui.py
+++ Statements/Gui.py
@@ -2,6 +2,7 @@ from Tkinter import Tk, OptionMenu, Label, LabelFrame, Text, Button, Frame
 from Tkinter import StringVar, Entry
 from Tkinter import RIDGE, END, TOP, LEFT, BOTH
 
+import Entry
 
 class Gui(object):
     """
@@ -15,7 +16,13 @@ class Gui(object):
     PADDING = 5
 
     def __init__(self, statement, entry):
+        """
+            instantiate a transaction window
+        """
+        self.date = entry.date
+        self.amount = entry.amount
         self.account = entry.account
+        self.description = entry.description
 
         self.root = Tk()
         self.root.title('Manual Annotation')
@@ -31,22 +38,23 @@ class Gui(object):
         # middle stack: entry details
         f = Frame(t)
         f1 = LabelFrame(f, text="Date")
-        Label(f1, text=entry.date).pack(padx=self.PADDING, pady=self.PADDING)
+        Label(f1, text=self.date).pack(padx=self.PADDING, pady=self.PADDING)
         f1.pack(side=LEFT, padx=self.PADDING)
 
         f1 = LabelFrame(f, text="Amount")
-        Label(f1, text=entry.amount).pack(padx=self.PADDING, pady=self.PADDING)
+        Label(f1, text=self.amount).pack(padx=self.PADDING, pady=self.PADDING)
         f1.pack(side=LEFT, padx=self.PADDING)
 
         f1 = LabelFrame(f, text="Account")
         acct = Text(f1, height=1, width=self.ACCT_WID)
-        acct.insert(END, entry.account)
+        if entry.account is not None:
+            acct.insert(END, self.account)
         acct.pack(padx=self.PADDING, pady=self.PADDING)
         f1.pack(side=LEFT, padx=self.PADDING)
 
         f1 = LabelFrame(f, text="Description")
         desc = Text(f1, height=1, width=self.DESC_WID)
-        desc.insert(END, entry.description)
+        desc.insert(END, self.description)
         desc.pack(padx=self.PADDING, pady=self.PADDING)
         f1.pack(side=LEFT, padx=self.PADDING)
         f.pack(pady=self.PADDING)
@@ -54,19 +62,31 @@ class Gui(object):
 
         # bottom stack: action buttons
         f = Frame(t)
-        Button(f, text="Accept").pack(side=LEFT, padx=self.PADDING)
-        Button(f, text="ACCOUUNT").pack(side=LEFT, padx=self.PADDING)
-        Button(f, text="AGGREGATE").pack(side=LEFT, padx=self.PADDING)
-        Button(f, text="Delete").pack(side=LEFT, padx=self.PADDING)
+        b = Button(f, text="Accept", command=self.accept)
+        b.pack(side=LEFT, padx=self.PADDING)
+        b = Button(f, text="ACCOUUNT")
+        b.pack(side=LEFT, padx=self.PADDING)
+        b = Button(f, text="AGGREGATE")
+        b.pack(side=LEFT, padx=self.PADDING)
+        b = Button(f, text="Delete",command=self.delete)
+        b.pack(side=LEFT, padx=self.PADDING)
         f.pack(padx=self.PADDING, pady=self.PADDING)
 
         # finalize
         t.pack(side=TOP)
 
+    def accept(self):
+        self.entry = Entry.Entry(self.date, self.amount, self.account, \
+            self.description)
+        self.root.quit()
+
+    def delete(self):
+        self.entry = None
+        self.root.quit()
 
     def mainloop(self):
+        """
+            process the dialog and return the constructed entry
+        """
         self.root.mainloop()
-
-        self.account = "***Account***"
-        self.aggregate = False
-        self.description = "***Description***"
+        return(self.entry)
