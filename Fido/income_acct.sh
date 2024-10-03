85aa1fdb603edc46120a312f4d92e36a620cd2f6
Mark.Kampe <mark.kampe@gmail.com>
2024-10-02 19:38:50 -0700

    update to use improved colsum

--- Fido/income_acct.sh
+++ Fido/income_acct.sh
@@ -11,7 +11,7 @@ else
     input="/home/markk/Downloads/positions.csv"
 fi
 
-account="retirement income"
+account="retirement-income"
 
 # so that the total variables survive after the loops are done
 shopt -s lastpipe
@@ -19,49 +19,27 @@ shopt -s lastpipe
 # generate a report of all of the debt instruments
 Income.py $input | grep "$account" > /tmp/ret_income
 
-# left padding for colsum
-pad="                    "
-line1="                    ----------"
-line2="   ------"
-
 # extract the money-market positions
 echo "$account: Money Market"
-grep "MMKT" /tmp/ret_income | cut -d' ' --complement -f1-2
-vtot=`grep "MMKT" /tmp/ret_income | colsum | awk '{print $1}'`
-vnum=`grep "MMKT" /tmp/ret_income | colsum | awk '{print $2}'`
-echo "$line1"
-echo "$pad" $vtot
+grep "MMKT" /tmp/ret_income | cut -d' ' --complement -f1-2 | colsum -v 
 
 # extract the CD positions
 echo
 echo "$account: CDs"
-grep "CD" /tmp/ret_income | cut -d' ' --complement -f1-2
-vtot=`grep "CD" /tmp/ret_income | colsum | awk '{print $1}'`
-vnum=`grep "CD" /tmp/ret_income | colsum | awk '{print $2}'`
-echo "$line1" "$line2"
-echo "$pad" $vtot "  " $vnum
+grep "CD" /tmp/ret_income | cut -d' ' --complement -f1-2 | colsum -v
 
 # extract the treasury positions
 echo
 echo "$account: Treasuries"
-grep "TREAS" /tmp/ret_income | cut -d' ' --complement -f1-2
-vtot=`grep "TREAS" /tmp/ret_income | colsum | awk '{print $1}'`
-vnum=`grep "TREAS" /tmp/ret_income | colsum | awk '{print $2}'`
-echo "$line1" "$line2"
-echo "$pad" $vtot "  " $vnum
+grep "TREAS" /tmp/ret_income | cut -d' ' --complement -f1-2 | colsum -v
 
 # extract the bond positions
 echo
 echo "$account: Other Bonds"
-grep "BOND" /tmp/ret_income | cut -d' ' --complement -f1-2
-vtot=`grep "BOND" /tmp/ret_income | colsum | awk '{print $1}'`
-vnum=`grep "BOND" /tmp/ret_income | colsum | awk '{print $2}'`
-echo "$line1" "$line2"
-echo "$pad" $vtot "    " $vnum
+grep "BOND" /tmp/ret_income | cut -d' ' --complement -f1-2 | colsum -v
 
-# sum treasuries + bonds
 echo
-vnum=`grep -e 'TREAS\|BOND' /tmp/ret_income | colsum | awk '{print $2}'`
-echo "TREAS+BOND: " "$pad" $vnum
+echo "$account: TREAS+BOND"
+grep "BOND\|TREAS" /tmp/ret_income | cut -d' ' --complement -f1-2 | colsum
 
 exit
