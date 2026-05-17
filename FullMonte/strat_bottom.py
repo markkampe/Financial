75560d038c7019742202fb8344f0e0aa40ae5f03
Mark Kampe <mark.kampe@gmail.com>
2026-05-17 12:46:54 -0700

    changed for new market (prices, enumerate all)

--- FullMonte/strat_bottom.py
+++ FullMonte/strat_bottom.py
@@ -1,112 +1,110 @@
+#!/usr/bin/python3
 """
-Purchasing Strategy: by at the bottom
+Purchasing Strategy: all-in/all-out
 """
 import statistics
-import sys
 import matplotlib.pyplot as plt
 from market import Market
 from buckets import bucketwidth, bucketize, distribution, value_offset
 from compound import compound_rate
 
 
-def strat_bottom(sequence, fractions, monthly=True):
+def strat_bottom(sequence, start, count, fractions, balance):
     """
-    Only buy at the lowest prices
-    :param sequence: list of (growth, dividend, interest) tupples
-    :param fractions(int): max number of lots
-           (we are willing to buy in at earlier lows)
-    :param monthly(bool): monthly (vs annual) sequence
+    Buy at the lowest point in the sequence
+    :param sequence: list of (price, dividend, interest) tupples
+    :param start(int): starting index to process
+    :param count(int): number of entries to process
+    :param fractions(int): number of lots
+           (if we are willingto buy at earlier lows)
+    :param balance(float): total investment amount
     :return (float): value of position at end of simulation
     """
+    # find the lowes prices in this market
+    buy_points = [-1] * fractions
+    for fract in range(fractions):
+        for i in range(count):
+            # ignore lows we've already found
+            if i in buy_points:
+                continue
 
-    # find the absolute bottom
-    buypoints = [0] * fractions
-    bottom = 666.0
-    market = 1.0
-    for i, (growth, dividend, interest) in enumerate(sequence):
-        market *= (1 + growth)
-        if market < bottom:
-            bottom = market
-            buypoints[0] = i
+            # is this a new low
+            price = sequence[start + i][0]
+            if buy_points[fract] == -1 or \
+               price < sequence[buy_points[fract]][0]:
+                buy_points[fract] = i
 
-    # find the buy points before that
-    for fract in range(1, fractions):
-        buypoints[fract] = 0
-        bottom = 666.0
-        market = 1.0
+            # print(buy_points)
 
-        # see if we have run out of opportunities
-        if buypoints[fract-1] == 0:
-            fractions = fract
-            break
+    # play through that purchase plan
+    shares = 0
+    purchase = balance/fractions
+    for i in range(count):
+        (price, dividend, interest) = sequence[start+i]
+        if i in buy_points:
+            shares += purchase/price
+            balance -= purchase
 
-        # find the next lowest earlier price
-        for i in range(buypoints[fract-1]):
-            (growth, dividend, interest) = sequence[i]
-            market *= (1 + growth)
-            if market < bottom:
-                bottom = market
-                buypoints[fract] = i
+        # every year reinvests dividends
+        if shares > 0:
+            shares += shares * dividend/price
 
-    # play the sequence, buying in at the bottom
-    in_market = 0.0     # start out with nothing in market
-    on_side = 1.0       # start out with everything on the side
+        # every year earns interest
+        balance += balance * interest/12
 
-    for i, (growth, dividend, interest) in enumerate(sequence):
-        # see if this is a buy point
-        if i in buypoints:
-            in_market += on_side/fractions
-            on_side -= on_side/fractions
-
-        # if we are monthly, scale the interest and dividends
-        if monthly:
-            dividend /= 12
-            interest /= 12
-
-        # figure out how much everybody made this period
-        in_market += in_market * (growth + dividend)
-        on_side += on_side * interest
-
-    return in_market + on_side
+    # figure out the final acount value
+    (price, _dividend, _interest) = sequence[start + count - 1]
+    return balance + (shares * price)
 
 
 # general simulation parameters
-NUM_RUNS = 200      # number of runs per model
+BALANCE = 1000.00   # initial balance
+START = 1970
+END = 2020
 NUM_YEARS = 20      # number of years to track results
 MY_NAME = "Bottom-Buying"
 OUTPUT = "Bottom.png"
 
 
 # pylint: disable=too-many-locals
-def main(random):
+def main():
     """
-    Only buy in at lows
-        run <num_runs> simulations
-        tracking output over 20 years
+    for a range of # lots
+        run simulations over all 20 year sequences
+            tracking total return
         plot a return distribution
     """
 
     # parameters specific to this continuous purchase model
-    title = ("Random" if random else "Real sequence") + " simulations of "
-    monthly = True     # monthly simulations
-    formats = ["w.", "r.", "y+", "g*", "co"]
+    title = "Real sequence simulations of "
 
     legends = []
-    simulator = Market(monthly=monthly)
+    formats = ["w.", "r.", "y+", "g*", "co"]
+
+    # test all possible sequences
+    simulator = Market(start=START, end=END)
+    count = NUM_YEARS * 12
+    last = len(simulator.data_points) - count
     for fractions in [1, 2, 3, 4]:
         results = []
-        # a statistically interesting number of runs
-        for _runs in range(NUM_RUNS * 2 if random else NUM_RUNS):
-            sequence = simulator.rates(length=NUM_YEARS*12, random=random)
-            results.append(strat_bottom(sequence, fractions, monthly))
+        samples = 0
+        for i in range(0, last):
+            sequence = simulator.data_points
+            results.append(strat_bottom(sequence, i, count, fractions,
+                           BALANCE))
+            samples += 1
 
         # summarize the results
         mean = sum(results) / len(results)
         sigma = statistics.stdev(results)
-        report = "{} over {} years in {} pieces: mean={:.2f}, sigma={:.2f}" + \
-                 ", {:.2f}%/y"
-        print(report.format(MY_NAME, NUM_YEARS, fractions, mean, sigma,
-              100*compound_rate(mean, NUM_YEARS)))
+        rate = compound_rate(mean/BALANCE, NUM_YEARS)
+        msg = MY_NAME
+        msg += f" over {NUM_YEARS} years"
+        msg += f" in {fractions} pieces:"
+        msg += f" ({samples} runs)"
+        msg += f": mean=${mean:,.0f}, sigma=${sigma:,.0f}"
+        msg += f", return={100*rate:.2f}%/y"
+        print(msg)
 
         # bucketize and display the results
         granularity = bucketwidth(results)
@@ -115,11 +113,11 @@ def main(random):
         (x_values, y_values) = distribution(buckets, granularity, offset)
 
         plt.plot(x_values, y_values, formats[fractions])
-        legends.append("fractions=" + str(fractions))
+        legends.append(f"fractions={fractions}")
 
     # put up the title, axes, and data
     plt.title(title + MY_NAME)
-    plt.xlabel(str(NUM_YEARS) + "-year return")
+    plt.xlabel(f" {NUM_YEARS}-year return on ${BALANCE:,.0f}")
     plt.ylabel("probability")
     plt.legend(legends)
     if OUTPUT is None:
@@ -131,7 +129,4 @@ def main(random):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "random":
-        main(True)
-    else:
-        main(False)
+    main()
