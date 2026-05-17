7b8dd64f0c551c6e9b5728708d2ea2312e2b8e48
Mark Kampe <mark.kampe@gmail.com>
2026-05-17 10:59:45 -0700

    work with new market.py
    	prices rather than growth rates
    	enumerate all sequences within data range

--- FullMonte/strat_all.py
+++ FullMonte/strat_all.py
@@ -1,7 +1,7 @@
+#!/usr/bin/python3
 """
 Purchasing Strategy: all-in/all-out
 """
-import sys
 import statistics
 import matplotlib.pyplot as plt
 from market import Market
@@ -9,68 +9,89 @@ from buckets import bucketwidth, bucketize, distribution, value_offset
 from compound import compound_rate
 
 
-def strat_all(sequence, play_it_safe, monthly=False):
+def strat_all(sequence, start, count, play_it_safe, balance):
     """
     All in the market or all out of the market
-    :param sequence: list of (growth, dividend, interest) tupples
+    :param sequence: list of (price, dividend, interest) tupples
+    :param start(int): starting index to process
+    :param count(int): number of entries to process
     :param play_it_safe(bool): are we in market or CDs
-    :param monthly(bool): monthly (vs annual) sequence
+    :param balance(float): total investment amount
     :return (float): value of position at end of simulation
     """
 
-    balance = 1.0
+    # start with our initial allocation
+    if play_it_safe:    # in CDs
+        shares = 0
+    else:               # in the market
+        (price, dividend, interest) = sequence[start]
+        shares = balance/price
+        balance = 0.00
 
-    for (growth, dividend, interest) in sequence:
-        # if we are monthly, scale the interest and dividends
-        if monthly:
-            dividend /= 12
-            interest /= 12
+    # play through all the months in the count
+    for i in range(count):
+        (price, dividend, interest) = sequence[start + i]
 
         if play_it_safe:
-            balance += balance * interest
+            # we just earn interest
+            balance += balance * interest/12
         else:
-            balance += balance * (growth + dividend)
+            # we reinvest (momthly) dividends
+            shares += dividend/price
 
-    return balance
+    # figure out the final acount value
+    (price, _dividend, _interest) = sequence[start + count - 1]
+    return balance + (shares * price)
 
 
 # general simulation parameters
-NUM_RUNS = 200      # number of runs per model
+BALANCE = 1000.00   # initial balance
+START = 1970
+END = 2020
 NUM_YEARS = 20      # number of years to track results
 MY_NAME = "All-In/Out"
 OUTPUT = "All.png"
 
 
 # pylint: disable=too-many-locals
-def main(random):
+def main():
     """
     For all-in and all-out
-        run <num_runs> simulations
-        tracking output over 20 years
+        run simulations over 20 year sequences
+            tracking total return
         plot a return distribution
     """
 
     # parameters specific to this continuous purchase model
-    title = ("Random" if random else "Real sequence") + " simulations of "
-    monthly = True     # annual simulations
+    title = "Real sequence simulations of "
 
     legends = []
-    simulator = Market(monthly=monthly)
+    simulator = Market(start=START, end=END)
 
     # purchases spread out over 1-5 years
     for in_cds in [True, False]:
         results = []
-        # a statistically interesting number of runs
-        for _runs in range(NUM_RUNS * 2 if random else NUM_RUNS):
-            sequence = simulator.rates(length=NUM_YEARS*12, random=random)
-            results.append(strat_all(sequence, in_cds, monthly))
+
+        # test all possible sequences
+        count = NUM_YEARS * 12
+        last = len(simulator.data_points) - count
+        samples = 0
+        for i in range(0, last):
+            sequence = simulator.data_points
+            results.append(strat_all(sequence, i, count, in_cds, 1000.00))
+            samples += 1
 
         # summarize the results
         mean = sum(results) / len(results)
         sigma = statistics.stdev(results)
-        report = "{} {}, {} years: mean={:.2f}, sigma={:.2f}, {:.2f}%/y"
-        print(report.format(MY_NAME, "CDs" if in_cds else "market",
-              NUM_YEARS, mean, sigma, 100*compound_rate(mean, NUM_YEARS)))
+        rate = compound_rate(mean/BALANCE, NUM_YEARS)
+        msg = MY_NAME
+        msg += " CDs,   " if in_cds else " market,"
+        msg += f" {NUM_YEARS} years"
+        msg += f" ({samples} runs)"
+        msg += f": mean=${mean:,.0f}, sigma=${sigma:,.0f}"
+        msg += f", return={100*rate:.2f}%/y"
+        print(msg)
 
         # bucketize and display the results
         granularity = bucketwidth(results)
@@ -83,7 +104,7 @@ def main(random):
 
     # put up the title, axes, and data
     plt.title(title + MY_NAME)
-    plt.xlabel(str(NUM_YEARS) + "-year return")
+    plt.xlabel(f" {NUM_YEARS}-year return on ${BALANCE:,.0f}")
     plt.ylabel("probability")
     plt.legend(legends)
     if OUTPUT is None:
@@ -95,7 +116,4 @@ def main(random):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1 and sys.argv[1] == "random":
-        main(True)
-    else:
-        main(False)
+    main()
