4f65dc187eaf1ec25e9a01117a7a1e33526e0e8c
Mark Kampe <mark.kampe@gmail.com>
2026-05-17 09:34:38 -0700

    now returns index/div prices (vs rates)

--- FullMonte/market.py
+++ FullMonte/market.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
 """
-A market return simulator, based on historical data
+A market data digester
 
 Author: Mark Kampe
 """
 import sys
-from random import randint
+from compound import compound_rate
 
 
 class Market:
     """
-    Digest a file of market quotes (w/date, dividend, price) data
-    Based on which we can return randomly chosen sequences of returns
+    Digest a file of market quotes, extract the entries between
+    a set of specified dates, and preoduce a simpler sequence
+    of monthly (price, dividend, interest rate) tupples.
     """
     input_file = ""     # file used for simulations
+    data_points = []
 
     def column(self, header, desired):
         """
@@ -32,18 +34,16 @@ class Market:
 
     # pylint: disable=too-many-arguments, too-many-locals
     def __init__(self, filename="sp500.csv",
-                 monthly=False,
                  start=1950, end=2020,
                  date_field="Date",
-                 price_field="SP500",       # inflation adjusted
-                 div_field="Dividend",      # inflation adjusted
-                 int_field="Long Interest Rate",
+                 price_field="SP500",       # inflation adjusted dollars
+                 div_field="Dividend",      # inflation adjusted dollars
+                 int_field="Long Interest Rate",    # percentage
                  date_format="y-m-d", ):
         """
         Instantiate a new simulator
 
         :param filename: name of file containing return data
-        :param monthly: monthly (vs annual) prices
         :param start: first year of data to be used
         :param end:   last year of data to be used
         :param date_field: column heading for dates
@@ -52,6 +52,7 @@ class Market:
         """
         # re-initialize the output array
         self.data_points = []
+        expected = (end + 1 - start) * 12
 
         # pylint: disable=R1732     # I don't want to indent the next 50 lines
         source = open(filename, "r", encoding='ascii')
@@ -71,17 +72,20 @@ class Market:
             month_col = fields.index('m')
 
         # process the entire file
-        prev = 0
         rate_sum = 0
         div_sum = 0
-        ret_sum = 0
         points = 0
+        line_num = 1
+        first_price = -666.0
+        last_price = -666.0
         for line in source:
+            line_num += 1
             fields = line.split(',')
 
             # make sure we have all of the expected data
             if (fields[date_col] == "" or fields[price_col] == "" or
                fields[div_col] == "" or fields[rate_col] == ""):
+                sys.stderr.write(f"{line_num}: missing data fields\n")
                 continue
 
             # pull out the date to see if it qualifies
@@ -92,121 +96,77 @@ class Market:
 
             # we will need to know the last price before our start
             price = float(fields[price_col])    # dollars
-            if year < start and (monthly or month == 1):
-                prev = price
 
             # see if this is within the requested range
             if start <= year <= end:
-                # see if we are doing only annual samples
-                if month != 1 and not monthly:
-                    continue
-
                 # pull out the price, dividend and interest rates
-                appreciation = price - prev         # $
-                div = float(fields[div_col])        # $ interp from 4Q totals
-                rate = float(fields[rate_col])      # % long rate
+                div = float(fields[div_col])        # this is an annual number
+                rate = float(fields[rate_col])      # long rate (as percentage)
 
                 # we record all of these as fractional values
-                tupple = (appreciation/prev, div/price, rate/100)
+                tupple = (price, div/12, rate/100)
                 self.data_points.append(tupple)
-                prev = price
 
                 # accumulate statistics for the whole sequence
-                ret_sum += appreciation/prev
+                if year == start and month == 1:
+                    first_price = price
+                elif year == end and month == 12:
+                    last_price = price
                 div_sum += div/price
                 rate_sum += rate/100
                 points += 1
 
         # summarize what we just read
-        period = "monthly" if monthly else "annual"
-        ret_pct = 100 * ret_sum / points * (12 if monthly else 1)
+        ret_pct = 100 * compound_rate(last_price/first_price, end + 1 - start)
         div_pct = 100 * div_sum / points
         rate_pct = 100 * rate_sum / points
         print(filename +
-              f"({start}-{end}): {points} {period} data points" +
+              f"({start}-{end}): {points}/{expected} momthly data points" +
               f", growth={ret_pct:3.1f}%" +
               f", div={div_pct:2.1f}%" +
               f", int(10y)={rate_pct:2.1f}%")
         source.close()
 
-    def rates(self, length=20, random=False):
-        """
-        return a (randomly chosen) list of market performance tupples
-
-        :param length: number of desired prices
-        :param random: random order (vs real sequences)
-        :return: list of price changes (appreciation, dividend, long rate)
-        """
-        size = len(self.data_points)
-        if random:
-            # return random values
-            return [self.data_points[randint(0, size - 1)]
-                    for i in range(0, length)]
-        # return consecutive values w/random starting point
-        first = randint(0, size - 1)
-        return [self.data_points[(first + i) % size]
-                for i in range(0, length)]
-
-    def chosen(self, first=0, length=0):
-        """
-        return a (contiguous) subset of the list
-
-        :param first: index of first desired element
-        :param length: number of desired elements
-        :return: list of data points
-        """
-        # figure out how many items to return
-        if length == 0:
-            length = len(self.data_points) - first
-
-        return [self.data_points[first + i]
-                for i in range(0, length)]
+    def t_dump(self, index):
+        """ format a tupple for printing """
+        (price, dividend, int_rate) = self.data_points[index]
+        return f"${price:9.2f}" +          \
+               f"\t${dividend*12:7.2f}" +   \
+               f"\t{int_rate*100:7.2f}%"
 
 
-def t_dump(app_rate, div_rate, int_rate):
-    """ format a tupple for printing """
-    return f"\t{app_rate*100:11.3f}%" +    \
-           f"\t{div_rate*100:7.2f}%" +   \
-           f"\t{int_rate*100:7.2f}%"
+def main(infile):
+    """
+    basic exercise of market data extraction
+
+    :param infile(string): neme of input file
+    """
+    # test data to be extracted and printed
+    year_start = 1950     # well after start
+    year_end = 2000       # well before end
+    year_test = 1970      # mid-range
+    num_years = 20        # months to print
+
+    simulator = Market(infile, start=year_start, end=year_end)
+    print()
+    print(f"Monthly sequenced return data from {infile}" +
+          f", beginning 01/01/{year_test}")
+    print("  (Compare the following with the same months from that file)")
+    print()
+
+    heading = "       date   \t     price\tdividend\tinterest\n" +\
+              "    ----------\t----------\t--------\t--------"
+    print(heading)
+    base = (year_test - year_start) * 12
+    for i in range(num_years):
+        year = int((base + i)/12) + year_start
+        month = 1 + ((base + i) % 12)
+
+        print(f"    {month:2}/01/{year:4}\t" + simulator.t_dump(base + i))
 
 
-# basic exerciser
 if __name__ == "__main__":
-    # pylint: disable=C0103     # pylint thinks infile is a constant!
     if len(sys.argv) > 1:
-        infile = sys.argv[1]
+        main(sys.argv[1])
     else:
-        infile = "sp500.csv"
-
-    START = 1950
-    END = 2020
-
-    heading = "\tappreciation\tdividend\tinterest\n" +\
-              "\t------------\t--------\t--------"
-
-    for by_month in [False, True]:
-        simulator = Market(infile, start=START, end=END, monthly=by_month)
-
-        for do_random in [False, True]:
-            print(("Monthly " if by_month else "Annual ") +
-                  ("random return" if do_random else "sequenced return") +
-                  " simulation based on " + infile)
-
-            sequence = simulator.rates(random=do_random)
-            print(heading)
-            for (delta, dividend, interest) in sequence:
-                print(t_dump(delta, dividend, interest))
-            print("")
-
-        print(("Monthly " if by_month else "Annual ") +
-              ("chosen sequence return") +
-              f" starting with {START+1}-01-01" +
-              " based on " + infile)
-
-        # choose a known sequence for easy testing
-        first_sample = 12 if by_month else 1
-        sequence = simulator.chosen(first_sample, length=20)
-        print(heading)
-        for (delta, dividend, interest) in sequence:
-            print(t_dump(delta, dividend, interest))
-        print("")
+        main("sp500.csv")
